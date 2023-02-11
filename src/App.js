f79d5a0477ab2bba2b4bb5b60e7258f9729755f0
Aryaman <sonichubhk@gmail.com>
2023-02-12 03:35:57 +0800

    Created sidebar and addition to feed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SideBar from "./components/SideBar";
-
-
+import FeedScreen from "./screens/FeedScreen";
+import MapScreen from "./screens/MapScreen";
+import Profile from "./screens/Profile";
+import './styles/App.css'
 
 const App = () => {
   return (
     <>
+      <BrowserRouter>
+        <SideBar>
+          <Routes>
+            <Route path="/feed" element={<FeedScreen />} />
+            <Route path="/mapscreen" element={<MapScreen />} />
+            <Route path="/profile" element={<Profile />} />
+          
+          </Routes>
+        </SideBar>
+      </BrowserRouter>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App.js";
-
-// Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
